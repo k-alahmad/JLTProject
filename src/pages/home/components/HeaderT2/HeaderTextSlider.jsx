3bf11770aac9fe09fb51d4d3b4f373fbfaffea71
moazabdulhalim <moazabdulhalim@gmail.com>
2023-07-23 13:17:06 +0300

    header not completed

--- src/pages/home/components/HeaderT2/HeaderTextSlider.jsx
+++ src/pages/home/components/HeaderT2/HeaderTextSlider.jsx
@@ -10,22 +10,22 @@ const HeaderTextSlider = () => {
       dots={false}
       autoplay
       autoplaySpeed={2000}
-      className="w-[90%] lg:w-[50%] absolute top-[30%] left-[10%] text-white h-full"
+      className="w-[90%] 2xl:w-[50%] absolute top-[30%] left-[5%] text-white h-full"
     >
       {data.textSlider.map((item, index) => {
         return (
-          <div key={index} className="cursor-pointer h-full">
+          <div key={index} className="cursor-pointer h-full ">
             <div className="flex items-end">
               <img
                 src={item.icon}
                 alt="icon"
-                className="h-40 w-40 mx-6  rounded "
+                className="h-36 w-36 lg:h-40 lg:w-40 mx-6  rounded "
               />
               <div>
-                <p className="text-[80px] font-bold text-white drop-shadow-2xl">
+                <p className="text-[70px] lg:text-[80px] font-bold text-white drop-shadow-2xl">
                   {item.title}
                 </p>
-                <p className="text-huge font-semibold uppercase drop-shadow-2xl">
+                <p className="text-bigger lg:text-huge font-semibold uppercase drop-shadow-2xl">
                   {item.subTitle}
                 </p>
               </div>
