e60bc299c0cc4f24b5040c33a3243499eeff7f61
moazabdulhalim <moazabdulhalim@gmail.com>
2023-07-24 15:30:16 +0300

    M-> 3d text refactor

--- src/components/UI/ComponentTitle/index.jsx
+++ src/components/UI/ComponentTitle/index.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import GradiantText from "../GradientText";
-const ComponentTitle = ({ title }) => {
-  // let splittedTitle = title.split(" ");
+// import GradiantText from "../GradientText";
+import Text3D from "./Text3D";
 
+const ComponentTitle = ({ title }) => {
   return (
-    <div className="relative text-bigger sm:text-[69px] flex justify-center items-center font-bold py-20 text-center md:text-start uppercase h-[200px]">
-      {/* <p className="grid grid-cols-2">
-        {splittedTitle.map((item, index) => {
-          return index % 2 == 0 ? (
-            <span className="px-2 ">{item}</span>
-          ) : (
-            <span className="px-2 text-primary">{item}</span>
-          );
-        })}
-      </p> */}
-      <GradiantText text={title} />
+    <div className="relative text-bigger sm:text-[69px] flex justify-center items-center font-bold py-20 text-center md:text-start uppercase">
+      {/* <GradiantText text={title} /> */}
+      <Text3D title={title} />
     </div>
   );
 };
--- src/components/UI/GradientText/index.jsx
+++ src/components/UI/GradientText/index.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import "./3D1.css";
 const GradientText = ({ text, className }) => {
-  let splittedTitle = text.split(" ");
   return (
     <div
       style={{
@@ -10,17 +8,16 @@ const GradientText = ({ text, className }) => {
       className="textContainer1 pb-2 px-4 md:px-4 max-sm:mx-2 rounded-[60px] rounded-tr-none"
     >
       <p
-        // style={{
-        //   background: "linear-gradient(-60deg, white 40%, #218DC2 50%)",
-        //   backgroundClip: "text",
-        //   WebkitBackgroundClip: "text",
-        //   color: "transparent",
-        //   WebkitTextFillColor: "transparent",
-        // }}
+        style={{
+          background: "linear-gradient(-60deg, white 40%, #218DC2 50%)",
+          backgroundClip: "text",
+          WebkitBackgroundClip: "text",
+          color: "transparent",
+          WebkitTextFillColor: "transparent",
+        }}
         className={`${className} `}
       >
-        <span className="text1"> {splittedTitle[0]} </span>
-        <span className="text2"> {splittedTitle[1] ?? ""} </span>
+        {text}
       </p>
     </div>
   );
