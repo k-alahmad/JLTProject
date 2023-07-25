32715aca26fea975b03f600a64e73922109644ce
Kanaan Alahmad <kanaan92@hotmail.com>
2023-07-25 19:37:27 +0400

    K-downlowd-Brouchur-fix-v1.0

--- src/components/Forms/RegisterForm/index.jsx
+++ src/components/Forms/RegisterForm/index.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { MdMail, MdPerson } from "react-icons/md";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/bootstrap.css";
-import Brochure from "../../../assets/pdf/330.pdf";
+import Brochure from "../../../assets/pdf/brochure.pdf";
 import {
 	hideModal,
 	register,
@@ -72,16 +72,16 @@ const RegisterForm = () => {
 		e.preventDefault();
 		let formData = new FormData(form.current);
 		try {
-			const response = await fetch(
-				"https://hooks.zapier.com/hooks/catch/12792925/312q4d0/",
-				{
-					method: "POST",
-					body: formData,
-					"Content-Type": "multipart/form-data",
-				},
-			);
-			const result = response.json();
-			console.log("Success:", result);
+			// const response = await fetch(
+			// 	"https://hooks.zapier.com/hooks/catch/12792925/312q4d0/",
+			// 	{
+			// 		method: "POST",
+			// 		body: formData,
+			// 		"Content-Type": "multipart/form-data",
+			// 	},
+			// );
+			// const result = response.json();
+			// console.log("Success:", result);
 			sendEmail(e);
 			if (downloadState && registerState) {
 				let alink = document.createElement("a");
